3dfc63f2ff4e5f7e2cfaf35dd5b9b30c16e31595
shendor <shendor1988@gmail.com>
2024-08-08 10:04:18 +0100

    Updated plugin to intellij 2024

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
+
+repositories {
+    mavenCentral()
+}
 
 plugins {
     java
-    kotlin("jvm") version "1.8.10"
-    id("org.jetbrains.intellij") version "1.15.0"
+    kotlin("jvm") version "2.0.0"
+    id("org.jetbrains.intellij") version "1.17.3"
 }
 val ideaVersion: String by project
 val jetbrainsPublishToken: String by project
@@ -13,55 +18,15 @@ val pluginVersion: String by project
 intellij {
     pluginName.set("cucumber-yaml")
     version.set(ideaVersion)
-    type.set("IC")
-    downloadSources.set(true)
-    instrumentCode.set(true)
-
-    // Gherkin plugin version: https://plugins.jetbrains.com/plugin/9164-gherkin/versions
-    val gherkinPlugin = when (ideaVersion) {
-        "2020.2" -> "gherkin:202.6397.21"
-        "2020.3" -> "gherkin:203.5981.155"
-        "2021.1" -> "gherkin:211.6693.111"
-        "2021.2" -> "gherkin:212.4746.57"
-        "2021.3" -> "gherkin:213.5744.223"
-        "2022.1" -> "gherkin:221.5080.126"
-        "2022.2" -> "gherkin:222.3345.118"
-        "2022.3" -> "gherkin:223.7571.113"
-        "2023.1" -> "gherkin:231.8109.91"
-        "2023.2" -> "gherkin:232.8660.88"
-        "2023.3" -> "gherkin:233.11799.165"
-        "201.8743.12" -> "gherkin:202.6397.21"
-        else -> ""
-    }
-    val yamlPlugin = when (ideaVersion) {
-        "2020.2" -> "org.jetbrains.plugins.yaml:202.6397.21"
-        "2020.3" -> "org.jetbrains.plugins.yaml:203.5981.37"
-        "2021.1" -> "org.jetbrains.plugins.yaml:211.6693.44"
-        "2021.2" -> "org.jetbrains.plugins.yaml:212.4746.16"
-        "2021.3" -> "org.jetbrains.plugins.yaml:213.5744.9"
-        "2022.1" -> "org.jetbrains.plugins.yaml:221.5080.106"
-        "2022.2" -> "org.jetbrains.plugins.yaml:222.3345.35"
-        "2022.3" -> "org.jetbrains.plugins.yaml:223.7571.59"
-        "2023.1" -> "org.jetbrains.plugins.yaml:231.8109.126"
-        "2023.2" -> "org.jetbrains.plugins.yaml:232.8660.88"
-        "2023.3" -> "org.jetbrains.plugins.yaml:233.11799.165"
-        else -> ""
-    }
-
     plugins.set(
         listOf(
             "com.intellij.java",
-            yamlPlugin,
+            "org.jetbrains.plugins.yaml:241.17011.41",
 //            "com.intellij.properties:223.7571.117",
             "Kotlin",
-            gherkinPlugin
+            "gherkin:241.17011.40"
         )
     )
-}
-
-repositories {
-    mavenCentral()
-    maven("https://www.jetbrains.com/intellij-repository/snapshots")
 }
 
 kotlin {
@@ -78,7 +43,7 @@ tasks {
     }
     publishPlugin {
         dependsOn("tag")
-        token.set(jetbrainsPublishToken)
+//        token.set(jetbrainsPublishToken)
         channels.set(listOf(version.toString().split('-').getOrElse(1) { "default" }.split('.').first()))
     }
     register<Exec>("publishTag") {
