448068a446b0bfbd5dad9e4c77cfc5f8558afbfa
shendor <shendor1988@gmail.com>
2026-01-07 19:46:20 +0000

    Updated references to files from yml

--- src/main/kotlin/org/shendor/cucumber/yaml/reference/YamlResourceFileReference.kt
+++ src/main/kotlin/org/shendor/cucumber/yaml/reference/YamlResourceFileReference.kt
@@ -16,15 +16,43 @@ class YamlResourceFileReference(element: PsiElement, textRange: TextRange) :
         val results = mutableListOf<ResolveResult>()
 
         val projectFileIndex = ProjectRootManager.getInstance(project).fileIndex
-        
-        val fileName = filePath.substringAfterLast('/')
-        val files = FilenameIndex.getFilesByName(project, fileName, GlobalSearchScope.projectScope(project))
+
+        val lastSlashIndex = filePath.lastIndexOf('/')
+        val directoryPath = if (lastSlashIndex != -1) filePath.substring(0, lastSlashIndex) else ""
+        val fileNamePattern = if (lastSlashIndex != -1) filePath.substring(lastSlashIndex + 1) else filePath
+
+        // Convert pattern to regex:
+        // 1. Escape dots
+        // 2. Replace <...> with .+
+        val regexString = fileNamePattern
+            .replace(".", "\\.")
+            .replace(Regex("<[^>]+>"), ".+")
+        val fileNameRegex = Regex("^$regexString$")
+
+        val files = mutableListOf<PsiFile>()
+        FilenameIndex.processAllFileNames({ fileName ->
+            if (fileNameRegex.matches(fileName)) {
+                val foundFiles = FilenameIndex.getFilesByName(project, fileName, GlobalSearchScope.projectScope(project))
+                files.addAll(foundFiles)
+            }
+            true
+        }, GlobalSearchScope.projectScope(project), null)
+
         for (file in files) {
             val virtualFile = file.virtualFile
             if (virtualFile != null) {
                 val normalizedPath = virtualFile.path.replace("\\", "/")
                 val normalizedFilePath = filePath.replace("\\", "/")
-                if (normalizedPath.endsWith(normalizedFilePath)) {
+                
+                // If there's a directory path, check if the file is in that directory
+                val matchesPath = if (directoryPath.isNotEmpty()) {
+                    val normalizedDirectoryPath = directoryPath.replace("\\", "/")
+                    normalizedPath.contains("/$normalizedDirectoryPath/") || normalizedPath.startsWith("$normalizedDirectoryPath/")
+                } else {
+                    true
+                }
+
+                if (matchesPath) {
                     // Check if it's in a source root (which includes resources)
                     if (projectFileIndex.isInSource(virtualFile) || projectFileIndex.isInSourceContent(virtualFile)) {
                         results.add(PsiElementResolveResult(file))
